0fee96d1ca8ea424ba7467a3f787d17ccbf4b868
Michał Jagieło <129671237+micjag1985@users.noreply.github.com>
2024-03-17 17:11:04 +0100

    Next course about JS

---
+++ fcc JavaScript Algorithms and Data Structures/Learn Basic JavaScript by Building a Role Playing Game.js
@@ -0,0 +1,13 @@
+// console.log("Hello World"); //Step 5
+let xp = 0; //Step 6, 7
+let health = 100; //Step 8
+let gold = 50; //Step 8
+let currentWeapon = 0; //Step 9
+let fighting; //Step 10
+let monsterHealth; //Step 11
+//let inventory = ["stick", "dagger", "sword"]; //Step 11, 12, 13
+let inventory = ["stick"]; //Step 14
+//let button1 = document.querySelector("#button1"); //Step 19
+const button1 = document.querySelector("#button1"); //Step 21
+const button2 = document.querySelector("#button2"); //Step 22
+const button3 = document.querySelector("#button3"); //Step 22
