b6ad4a52b5f6ce39fd4a1be4fb4a31a14cb4d1fa
Michał Jagieło <129671237+micjag1985@users.noreply.github.com>
2024-04-02 21:27:35 +0200

    Second task

---
+++ fcc JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter.js
@@ -0,0 +1,5 @@
+// Step 14
+const calorieCounter = document.getElementById('calorie-counter');
+// Step 15
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
